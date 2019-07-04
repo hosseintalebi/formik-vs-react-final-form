80775f11e236d7e8013e274158337b1e0bd902b5
Hossein <hossein.talebi@farmersedge.ca>
2019-07-03 20:49:57 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "@material/button/dist/mdc.button.min.css";
+import "@material/textfield/dist/mdc.textfield.min.css";
+import "@material/select/dist/mdc.select.min.css";
+
+import Forms from "./components/Forms";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Forms />
     </div>
   );
 }
