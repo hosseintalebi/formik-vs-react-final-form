e2f8887cb50909a12185229d3ba18e849480b243
Hossein <hossein.talebi@farmersedge.ca>
2019-07-29 11:53:09 -0600

    Updates

--- precache-manifest.e2cd8cb4cde09cdf95acc61a2c885a2a.js
+++ precache-manifest.e2cd8cb4cde09cdf95acc61a2c885a2a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac9f4d82a8172d5f3356a6cde96a15c5",
+    "revision": "3d336d34ca520b3e6b8424df1d3bd1c0",
     "url": "/formik-vs-react-final-form/index.html"
   },
   {
-    "revision": "f5c1889b9046c280535b",
+    "revision": "0a76fb990ee1d6f9b8a3",
     "url": "/formik-vs-react-final-form/static/css/2.b46bc8db.chunk.css"
   },
   {
-    "revision": "653ecce77f7b535e603f",
+    "revision": "3683068bcbbbc78ffbeb",
     "url": "/formik-vs-react-final-form/static/css/main.e3916421.chunk.css"
   },
   {
-    "revision": "f5c1889b9046c280535b",
-    "url": "/formik-vs-react-final-form/static/js/2.8e4a1c9c.chunk.js"
+    "revision": "0a76fb990ee1d6f9b8a3",
+    "url": "/formik-vs-react-final-form/static/js/2.97e6eb4d.chunk.js"
   },
   {
-    "revision": "653ecce77f7b535e603f",
-    "url": "/formik-vs-react-final-form/static/js/main.3a129600.chunk.js"
+    "revision": "3683068bcbbbc78ffbeb",
+    "url": "/formik-vs-react-final-form/static/js/main.394c539d.chunk.js"
   },
   {
     "revision": "319d1ae6da1515038346",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/formik-vs-react-final-form/precache-manifest.3e72e10c3e6206d5df2ef7ed3a660131.js"
+  "/formik-vs-react-final-form/precache-manifest.e2cd8cb4cde09cdf95acc61a2c885a2a.js"
 );
 
 self.addEventListener('message', (event) => {
